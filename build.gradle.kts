97b658878a6fa582f5324a00701f8cde840ef95d
Anže Sodja <asodja@gradle.com>
2022-03-25 11:56:46 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.8.1")
+    implementation("org.springframework:spring-web:4.3.7.RELEASE")
+}
